81cd0b40ea5b72d5e31ac25078a40ed23b52b78a
oliversteidel <67387391+oliversteidel@users.noreply.github.com>
2021-03-29 12:29:29 +0200

    init countries app

---
+++ countries/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <header class="header flex ai-c jc-sb">
+      <h1 class="header__title">Where in the world?</h1>
+      <div class="header__theme-switch-wrapper flex ai-c">
+        <img src="./assets/icon-sun.svg" alt="" class="header__icon">
+        <p class="header__theme-text">Light Mode</p>
+      </div>
+    </header>
+    <div class="container">
+      <Searchbar />
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import Searchbar from "./components/Searchbar.vue";
+export default {
+  
+  name: "App",
+  components: {
+    Searchbar
+  },
+};
+</script>
+
+<style lang="scss">
+@import "./style/_globals.scss";
+
+#app {
+  background: $bg-dark;
+  height: 100vh;  
+}
+
+.header {
+  width: 100%;
+  max-width: 90rem;
+  height: 5rem;
+  padding: 0 1rem; 
+  background: $elements-dark;
+  box-shadow: 0 0 15px rgba(black, 0.1);
+
+  &__title {
+    font-size: 0.875rem;
+    font-weight: $extra-bold;
+    color: $text-dark;
+  }
+
+  &__icon {
+    width: 0.75rem;
+    height: 0.75rem;
+  }
+
+  &__theme-text {
+    font-size: 0.875rem;
+    font-weight: $semi-bold;
+    color: $text-dark;
+    margin-left: 0.75rem;
+  }
+}
+
+.container {
+  padding: 0 1rem;
+}
+</style>
---
+++ countries/src/components/Searchbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="searchbar-wrapper flex ai-c">
+    <i class="fas fa-search"></i>
+    <input
+      type="search"
+      name="searchbar"
+      id="searchbar"
+      placeholder="Search for a country..."
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Searchbar",
+};
+</script>
+
+<style lang="scss" scoped>
+.searchbar-wrapper {
+  width: 100%;
+  max-width: 30rem;
+  height: 3.625rem;
+  margin-top: 1.5rem;
+  padding: 0 1.875rem;
+  background: $elements-dark;
+  border-radius: 5px;
+
+  .fas {
+    color: $text-dark;
+  }
+
+  input {
+    border: none;
+    background: transparent;
+    color: $text-dark;
+    font-family: inherit;
+    margin-left: 1.625rem;
+  }
+
+  input::placeholder {
+      opacity: 1;
+  }
+}
+</style>
+
