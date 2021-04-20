53579a1f4e1ff9e1103d4189d16b570eb6a305b7
oliversteidel <67387391+oliversteidel@users.noreply.github.com>
2021-04-20 13:05:18 +0200

    easybank website setup and init

---
+++ easybank/gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+// const sassPartialsImported = require('gulp-sass-partials-imported');
+const autoprefixer = require('gulp-autoprefixer');
+
+// let scss_dir = './scss';
+// let includePaths = ['./scss'];
+
+function style() {
+    // 1. where is my scss file
+    return gulp.src('./scss/**/*.scss')
+    //.pipe(sassPartialsImported(scss_dir, includePaths))
+
+    // 2. pass that file through sass compiler
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer('last 2 versions'))
+
+    // 3. where do I save the compiled CSS?
+    .pipe(gulp.dest('./css'))
+    // 4. stream changes to all browsers
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: '.',
+            index: "index.html"
+        }
+    });
+    gulp.watch('./scss/**/*.scss', style);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('./js/*.js').on('change', browserSync.reload);    
+}
+
+
+
+exports.style = style;
+exports.watch = watch;
