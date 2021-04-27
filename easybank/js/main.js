06dc3cab0890ba591c838017d8b21e10a9ae3e97
oliversteidel <67387391+oliversteidel@users.noreply.github.com>
2021-04-27 12:14:43 +0200

    change burger-btn-img on click

--- easybank/js/main.js
+++ easybank/js/main.js
@@ -1,16 +1,19 @@
 const navBtn = document.getElementById('burger');
 const navList = document.getElementById('nav-list');
 const navListBg = document.getElementById('nav-toggle-bg');
+const burgerImg = document.getElementById('burger-img');
 let navOpen = false;
 
 navBtn.addEventListener('click', function () {
     if (!navOpen) {
+        burgerImg.setAttribute('src', './images/icon-close.svg')
         gsap.to(navListBg, { duration: 0.4, height: 600 });
         gsap.to(navList, { duration: 0.5, height: 260 });
         navOpen = true;
     }else{
         gsap.to(navListBg, { duration: 0.6, height: 0 });
         gsap.to(navList, { duration: 0.5, height: 0 });
+        burgerImg.setAttribute('src', './images/icon-hamburger.svg')
         navOpen = false;
     };
 });
