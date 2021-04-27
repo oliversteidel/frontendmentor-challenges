5b6a8fc6ea075daf2ff81d9caa911af1e0e4a86a
oliversteidel <67387391+oliversteidel@users.noreply.github.com>
2021-04-27 20:43:15 +0200

    hover animation on nav links
    fixed: hero title collapsing with hero img
    issues: nav link hover anim. on wrong pos after resizing window

--- easybank/js/main.js
+++ easybank/js/main.js
@@ -12,7 +12,7 @@ navBtn.addEventListener('click', function () {
         gsap.to(navListBg, { duration: 0.4, height: 600 });
         gsap.to(navList, { duration: 0.5, height: 260 });
         navOpen = true;
-    }else{
+    } else {
         gsap.to(navListBg, { duration: 0.6, height: 0 });
         gsap.to(navList, { duration: 0.5, height: 0 });
         burgerImg.setAttribute('src', './images/icon-hamburger.svg')
@@ -20,22 +20,45 @@ navBtn.addEventListener('click', function () {
     };
 });
 
-
 // on desktop devices show marker below nav link (bottom of navbar) on hover
 // marker grows in width on mouseover navlink
 // marker follows mouse on hovering to neigbour navlinks
+// marker with shrinks to 0 on mouseleave
 
 const marker = document.querySelector('.nav__hover-marker');
 const navLinks = document.querySelectorAll('.nav__link');
-let linksContainerPos = navList.offsetLeft;
-let linkPosX;
 let markerWidth = 62;
+let linksContainerPos = navList.offsetLeft;
+let linkPosX = navLinks[0].offsetLeft + linksContainerPos + markerWidth;
+
+
+// set initial marker position
+gsap.set(marker, { x: linkPosX });
+
+// set marker after resizing the window
+window.addEventListener('resize', function() {
+    linkPosX = navLinks[0].offsetLeft + navList.offsetLeft + markerWidth;
+    gsap.set(marker, { x: linkPosX });    
+});
+
+// marker grows in width on mouseenter navlink
+navLinks.forEach(el => {
+    el.addEventListener('mouseenter', function (e) {
+        linkPosX = e.target.offsetLeft + linksContainerPos + markerWidth;        
+        gsap.to(marker, { duration: 0.3, width: 52 });
+    });
+});
 
 // marker follows nav link on which the cursor is
 navLinks.forEach(el => {
-    el.addEventListener('mouseover', function(e) {
+    el.addEventListener('mouseover', function (e) {
         linkPosX = e.target.offsetLeft + linksContainerPos + markerWidth;
-        gsap.to(marker, {x: linkPosX});
+        gsap.to(marker, { duration: 0.3, x: linkPosX });
     });
+});
+
+// marker disappears (set with to 0) on mouseleave nav-list(ul) 
+navList.addEventListener('mouseleave', function () {
+    gsap.to(marker, { duration: 0.3, width: 0 });
 });
 
